4a51bf61b6f7423352f1495e8770b5efb0c5ddae
Tiago Kieliger <tiago.kieliger@gmail.com>
2017-10-22 09:36:30 +0200

    Improved the defence.sh code, it now only warns about possible attacks on the wifi to which the device is currently connected

--- defence.sh
+++ defence.sh
@@ -4,45 +4,44 @@
 #given some preferred essid and MAC, if another MAC with the same SSID exists
 #a notification warns the user
 
-while true; do
-	mapfile -t myArray < authorised.list
+mapfile -t myArray < authorised.list
+while true
+do
 	index=1
-	for j in `seq 0 $((${myArray[0]}-1))`; do
-		echo "j is $j"
+	for j in $(seq 0 $((myArray[0]-1)))
+	do
 		count=0
 		SSID=${myArray[index]}
 		((++index))
+		connectedSSID=$(iwgetid -r)	
 		nbAuthorisedMacs=${myArray[index]}
-		echo "Nb authorised macs is $nbAuthorisedMacs"
 		((++index))
-		echo "SSID is $SSID"
-		array=( $(sudo iwlist wlp4s0 scan | grep 'Address\|ESSID:' | grep -B 1 "\"${SSID}\"") )
-		for i in "${array[@]}"
-		do
-			#echo "$i"
-			if [ $(($count%7)) -eq 4 ]
-			then
-				#echo "${i}"
-				problem="YES"
-				for k in `seq $index $(($index+$nbAuthorisedMacs-1))`; do
-					
-					if [ ${myArray[k]} == "$i" ]
+		if [ "$SSID" == "$connectedSSID" ]
+		then
+			array=( $(sudo iwlist wlp4s0 scan | grep 'Address\|ESSID:' | grep -B 1 "\"${SSID}\"") )
+			for i in "${array[@]}"
+			do
+				if [ $((count%7)) -eq 4 ]
+				then
+					#echo "${i}"
+					problem="YES"
+					for k in $(seq $index $((index+nbAuthorisedMacs-1)))
+				        do
+						if [ "${myArray[k]}" == "$i" ]
 						then
-						problem="NO"
+							problem="NO"
 						fi
-				done
-				if [ "$problem" != "NO" ]
+					done
+					if [ "$problem" != "NO" ] && [ "${i}" != "ESSID:\"$SSID\"" ]
 					then
-						if [ ${i} != "ESSID:"$SSID"" ]
-							then
-							notify-send "Warning, wifi may be compromised"
-							echo "Warning, unexpeced MAC : ${i}"
-						fi				
-				fi
-			fi	
-			((++count))
-		done
-		index=$(($index+$nbAuthorisedMacs))
+						notify-send "Warning, wifi ${SSID} may be compromised"
+						echo "Warning, unexpeced MAC : ${i}"				
+					fi
+				fi	
+				((++count))
+			done
+		fi
+		index=$((index+nbAuthorisedMacs))
 	done
 	sleep 30s
 done
